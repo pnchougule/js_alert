e8bde5a6eed13cf818e438750fe56f936b352435
Poonam Natha Chougule <59753262+pnchougule@users.noreply.github.com>
2023-01-17 11:38:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("btn2").onclick = function (){
+    alert("Button 2 is clicked");
+}
+
+
+document.getElementById("btn3").addEventListener("click",function (){
+    alert("Button 3 is clicked");
+});
